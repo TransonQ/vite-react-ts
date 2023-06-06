6ade3f66fe1e788cfecf9122d3940c58d9c39298
TransonQ <quanscheng@qq.com>
2023-06-07 00:34:26 +0800

    chore:  vite路径别名

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,23 @@
-import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite'
+
+import { dirname, resolve } from 'path'
+import { fileURLToPath } from 'url'
+
+const _dirname =
+  typeof __dirname !== 'undefined'
+    ? __dirname
+    : dirname(fileURLToPath(import.meta.url))
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: 'build',
+  },
+  resolve: {
+    alias: {
+      '@': resolve(_dirname, 'src'),
+    },
+  },
 })
